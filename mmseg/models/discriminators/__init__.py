cfc50988003a4fd8718a2098c0277121940065bc
Shuchang Lyu <shuchanglv_ww@163.com>
2025-04-13 00:28:46 +0800

    Update __init__.py

--- mmseg/models/discriminators/__init__.py
+++ mmseg/models/discriminators/__init__.py
@@ -1,6 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .AdapSeg import AdapSegDiscriminator
+## added by LYU: 2025/04/10
+from .wgangp import WGANGPDiscriminator
 
 __all__ = [
-    'AdapSegDiscriminator'
+    'AdapSegDiscriminator', 'WGANGPDiscriminator'
 ]
